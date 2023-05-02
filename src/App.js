e51de203f7b9e514bcd541d3f43d64196f071bb2
Tavaszi Csaba <74116832+Tavaszka1985@users.noreply.github.com>
2023-05-02 14:51:27 +0300

    02.05.2023

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
-function App() {
+const App = () => {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((previousUserList) => {
+      return [
+        ...previousUserList,
+        { id: Math.random().toString(), name: userName, age: userAge },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
-}
+};
 
 export default App;
